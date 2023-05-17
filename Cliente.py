7ee5fba7d766562efa1f3dea51a52dda2e199297
aglzlpz <antoniogl.2003@outlook.com>
2023-05-17 22:18:53 +0200

    DEFINITIVO
    Hay que desactivar el firewall en redes privadas. Se ha añadido un algoritmo de encriptación simétrica (Cifrado César) para mayor seguridad.

--- Cliente.py
+++ Cliente.py
@@ -2,40 +2,69 @@ import socket
 import threading
 
 ipclient=input("Escribe la IP del servidor: ")
-port=8000
+port=8000                  #PUERTO DEL SERVIDOR QUE ATENDERÁ PETICIONES
 
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#SE CREA CONEXIÓN TCP/IP ENTRE CLIENTE Y SERVIDOR
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)        
 
 client.connect((ipclient,port))
 
 
-nombre=input("Escribe tu nombre de usuario: ")
+nombre=input("Escribe tu nombre de usuario: ")      #SE PREGUNTA A USUARIO SU NOMBRE
 
-def recibir():
-    while True:
+def encriptar(mensaje, desplazamiento):           #ENCRIPTACIÓN CIFRADO CÉSAR
+    mensaje_encriptado = ""
+    for caracter in mensaje:
+        if str(caracter).isalpha():
+            ascii_inicial = ord('a') if str(caracter).islower() else ord('A')
+            ascii_encriptado = (ord(str(caracter)) - ascii_inicial + int(desplazamiento)) % 26 + ascii_inicial      #FÓRMULA ENCRIPTADO
+            caracter_encriptado = chr(ascii_encriptado)
+            mensaje_encriptado += caracter_encriptado
+        else:
+            mensaje_encriptado += str(caracter)
+    return mensaje_encriptado
+
+
+def decrypt(mensaje_encriptado, desplazamiento):    #DESENCRIPTACIÓN CIFRADO CÉSAR
+    mensaje_desencriptado = ""
+    for caracter in mensaje_encriptado:
+        if caracter.isalpha():
+            ascii_inicial = ord('a') if caracter.islower() else ord('A')
+            ascii_desencriptado = (ord(caracter) - ascii_inicial - desplazamiento) % 26 + ascii_inicial     
+            caracter_desencriptado = chr(ascii_desencriptado)
+            mensaje_desencriptado += caracter_desencriptado
+        else:
+            mensaje_desencriptado += caracter
+    return mensaje_desencriptado
+
+
+def recibir():          #FUNCIÓN PARA RECIBIR MENSAJES DESDE EL SERVIDOR
+    while True:         
         try:
-            msj=client.recv(1024).decode('utf-8')
-            print(msj)
+            msj=client.recv(1024).decode('utf-8')       #SE RECIBEN MENSAJES DE MÁXIMO 1024 BYTES Y SE DECODIFICAN USANDO UTF-8
+            print(decrypt(msj,10))                      #SE DESENCRIPTA CON CIFRADO CÉSAR
 
-        except:
+        except:                                         #EN CASO DE QUE HAYA ALGÚN ERROR...
             print("Desconectando del servidor...")
             client.close()
             break
 
 
-hilorecibir = threading.Thread(target=recibir)
-hilorecibir.start()
+hilorecibir = threading.Thread(target=recibir)          #UN HILO SE EJECUTA SIEMPRE Y SE ENCARGA DE RECIBIR MENSAJES
+hilorecibir.start()                                     #SE INICIA EL HILO
 
-while True:
-    msj=input('>')
-    if msj:
-        if msj.upper()=="FIN":
+
+while True:                          #CLIENTE ESCUCHANDO SIEMPRE A USUARIO PARA MANDAR MENSAJES AL SERVIDOR
+    msj=input('>')                   #MENSAJE QUE ESCRIBE EL USUARIO
+    if msj:                          #EN CASO DE QUE EL USUARIO HAYA ESCRITO UN MENSAJE
+        if msj.upper()=="FIN":       #SI USUARIO ESCRIBE FIN --> SE TERMINA LA CONEXIÓN
             client.close()
-            
             break
-        else:
-            client.sendall(f"{nombre}: {msj}".encode("utf-8"))
-        
 
-#hiloenviar.start()
-#hiloenviar = threading.Thread(target=enviar)
+        else:
+            msjsinencriptar=f"{nombre}: {msj}"      #SE CONCATENA EL NOMBRE DE USUARIO CON EL MENSAJE
+            msjencriptado=encriptar(msjsinencriptar,10).encode("utf-8")     #SE ENCRIPTA EL MENSAJE USANDO CIFRADO CÉSAR CON DESPLAZAMIENTO 10
+        #    print(msjencriptado)        #COMPROBACIÓN DE QUE SÍ ENCRIPTA
+            client.sendall(msjencriptado)           #SE ENVÍA EL MENSAJE ENCRIPTADO AL SERVIDOR
+        
--- Servidor.py
+++ Servidor.py
@@ -2,47 +2,47 @@ import socket
 import threading as threading
 
 
-host=socket.gethostbyname(socket.gethostname())
-port=8000
+host=socket.gethostbyname(socket.gethostname())         #IP PRIVADA DEL SERVIDOR
+port=8000                                               #PUERTO DEL SERVIDOR DONDE SE ATIENDEN PETICIONES
 
-socketserver=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socketserver=socket.socket(socket.AF_INET,socket.SOCK_STREAM)       #SE CREA LA CONEXIÓN TCP/IP
 socketserver.bind((host,port))
-socketserver.listen()
+socketserver.listen()                  #EL SERVIDOR EMPIEZA A ESCUCHAR DESDE EL PUERTO PARA ATENDER LAS CONEXIONES DEL CLIENTE
     
-print(f"El servidor se encuentra en: {socket.gethostbyname(socket.gethostname())}:{port}")      #SE DA IP:PUERTO DEL SERVIDOR
+print(f"El servidor se encuentra en: {socket.gethostbyname(socket.gethostname())}")      #SE DA IP DEL SERVIDOR
     
 print("Esperando conexión con clientes...")
 
-clientes=[]
+clientes=[]             #LISTA DE CLIENTES
     
 
-def broadcast(msj,remitente):
-    for client in clientes:
-        if client != remitente:
+def broadcast(msj,remitente):           #FUNCIÓN QUE ENVÍA MENSAJE A TODOS LOS CLIENTES MENOS A REMITENTE
+    for client in clientes:     
+        if client != remitente:         #SI EL CLIENTE NO ES EL REMITENTE
             try:
-                client.sendall(msj.encode("utf-8"))
+                client.sendall(msj.encode("utf-8"))         #SE ENVÍA EL MENSAJE
             except:
-                clientes.remove(client)
+                clientes.remove(client)                     #SI HAY ALGÚN ERROR, SE ELIMINA AL CLIENTE DE LA LISTA DE CLIENTES
                 print(f"Desconectando a {address} por un error en la conexión")
 
-def recibiryenviar(client,address):
-    clientes.append(client)
+def recibiryenviar(client,address):     #FUNCIÓN QUE ESTABLECE CONEXIÓN CON UN CLIENTE Y SE ENCARGA DE DISTRIBUIR EL TRÁFICO
+    clientes.append(client)             #SE AÑADE EL NUEVO CLIENTE A LA LISTA DE CLIENTES
     print(f"Conexión establecida desde {address}") 
     
-    while True:
+    while True:                         #BUCLE INFINITO
         try:
-            msj=client.recv(1024).decode("utf-8")
-            if msj:
+            msj=client.recv(1024).decode("utf-8")           #SE RECIBEN LOS MENSAJES ENCRIPTADOS DEL CLIENTE (MÁXIMO 1024 BYTES)
+            if msj:                                         #SI HAY UN MENSAJE
                 print(f"Mensaje recibido desde {address}: {msj}")
-                broadcast(msj,client)
+                broadcast(msj,client)                       #EL MENSAJE RECIBIDO POR EL SERVIDOR SE ENVÍA A TODOS LOS CLIENTES
         except:
             broadcast(f"{client} se ha desconectado", client)
             print(f"Conexión cerrada desde {address}")
-            clientes.remove(client)
-            break
+            clientes.remove(client)         #EN CASO DE ERROR SE AVISA DE LA DESCONEXIÓN Y SE ELIMINA AL CLIENTE DE LA LISTA
+            break                           #SE ACABA EL BUCLE DE
 
 
-while True:
-    client,address=socketserver.accept()
-    hilo = threading.Thread(target=recibiryenviar,args=(client,address))
-    hilo.start()
+while True:                 #POR SIEMPRE
+    client,address=socketserver.accept()            #ACEPTAR LAS CONEXIONES QUE LLEGAN AL SOCKET
+    hilo = threading.Thread(target=recibiryenviar,args=(client,address))        #SE CREA UN HILO POR CADA CLIENTE QUE EJECUTA LA FUNCION RECIBIRYENVIAR()
+    hilo.start()            #SE EJECUTA EL HILO
